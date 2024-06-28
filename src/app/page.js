9686a2cbdb00d90336291a02ae45ffe1f30213d6
Vu Hoang Anh <vha68360@gmail.com>
2024-06-28 11:21:22 +0700

    #

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,3 @@
-import Image from "next/image";
-export default function Home() {
-  return (
-    <main>
-      {/* <div className="container-left">
-        <Image src='/images/login.png' alt="Login" width={708} height={900}/>
-      </div>
-      <div className="container-right"></div> */}
-    </main>
-  );
-}
+import page from "./(vendor)/home/page";
+
+export default page
