e5a4b3896e61669aa80e30697a8107204b49edf9
Vu Hoang Anh <vha68360@gmail.com>
2024-07-01 10:18:15 +0700

    #

--- src/app/worker/home/page.js
+++ src/app/worker/home/page.js
@@ -25,8 +25,8 @@ const page = () => {
                 </div>
                 <div className="wk-banner-content">
                 <div className="wk-banner-left">
-                    <h1 className="wk-ibm-plex-mono-bold">Work and Earn Every minute</h1>
-                    <p className="wk-ibm-plex-mono-light">WorkEvo is a Web3 app that rewards users for tasks completion</p>
+                    <h1 className="ibm-plex-mono-bold">Work and Earn Every minute</h1>
+                    <p className="ibm-plex-mono-light">WorkEvo is a Web3 app that rewards users for tasks completion</p>
                     <div className="wk-left-btn">
                         <div className="wk-con-btn">
                             <a href="#" className="wk-con-us ibm-plex-mono-regular" >Become a Worker </a>
@@ -44,23 +44,23 @@ const page = () => {
 
                <div className="wk-communiti-1">
                     <div className="wk-com1-content">
-                        <h1 className="wk-ibm-plex-mono-bold">Join very active worker communities</h1>
+                        <h1 className="ibm-plex-mono-bold">Join very active worker communities</h1>
                         <div className="wk-com1-list">
                             <div className="wk-com1-item">
-                                <div className="wk-ibm-plex-mono-semibold com1-num">100K+</div>
-                                <div className="wk-ibm-plex-mono-medium com1-title">Worker</div>
+                                <div className="ibm-plex-mono-semibold wk-com1-num">100K+</div>
+                                <div className="ibm-plex-mono-medium wk-com1-title">Worker</div>
                             </div>
                             <div className="wk-com1-item">
-                                <div className="wk-ibm-plex-mono-semibold com1-num">100,000+</div>
-                                <div className="wk-ibm-plex-mono-medium com1-title">Tasks</div>
+                                <div className="ibm-plex-mono-semibold wk-com1-num">100,000+</div>
+                                <div className="ibm-plex-mono-medium wk-com1-title">Tasks</div>
                             </div>
                             <div className="wk-com1-item">
-                                <div className="wk-ibm-plex-mono-semibold com1-num">$12,7M</div>
-                                <div className="wk-ibm-plex-mono-medium com1-title">Earned</div>
+                                <div className="ibm-plex-mono-semibold wk-com1-num">$12,7M</div>
+                                <div className="ibm-plex-mono-medium wk-com1-title">Earned</div>
                             </div>
                             <div className="wk-com1-item">
-                                <div className="wk-ibm-plex-mono-semibold com1-num">1000+</div>
-                                <div className="wk-ibm-plex-mono-medium com1-title">New task per day</div>
+                                <div className="ibm-plex-mono-semibold wk-com1-num">1000+</div>
+                                <div className="ibm-plex-mono-medium wk-com1-title">New task per day</div>
                             </div>
                         </div>
                         <a href="#" className="wk-com1-btn ibm-plex-mono-medium">Join Community</a>
@@ -68,9 +68,9 @@ const page = () => {
                </div>
                <div className="wk-about-container">
                     <div className="wk-about-left">
-                        <h1 className="wk-ibm-plex-mono-bold">About WorkEvo</h1>
-                        <p className="wk-ibm-plex-mono-light">Users earn money in their free time by completing tasks we offer. Tasks include data and image censorship, each task will take from 5 -15 minutes, and you will receive a number of WEA corresponding to the difficulty of each task.</p>
-                        <p className="wk-ibm-plex-mono-light">WorkEvo platform has more than 100 new tasks every day for everyone, you can preview tasks and rewards to decide to start working.</p>
+                        <h1 className="ibm-plex-mono-bold">About WorkEvo</h1>
+                        <p className="ibm-plex-mono-light">Users earn money in their free time by completing tasks we offer. Tasks include data and image censorship, each task will take from 5 -15 minutes, and you will receive a number of WEA corresponding to the difficulty of each task.</p>
+                        <p className="ibm-plex-mono-light">WorkEvo platform has more than 100 new tasks every day for everyone, you can preview tasks and rewards to decide to start working.</p>
                         <a href="#" className="wk-about-btn ibm-plex-mono-regular">Whitepaper</a>
                     </div>
                     <div className="wk-about-right">
@@ -79,26 +79,26 @@ const page = () => {
                </div>
                <div className="wk-task-container">
                     <div className="wk-task-header">
-                        <h1 className="wk-ibm-plex-mono-bold task-title">Work to Earn</h1>
+                        <h1 className="ibm-plex-mono-bold wk-task-title">Work to Earn</h1>
                     </div>
                     <div className="wk-task-content">
                         <div className="wk-task-top">
                             <div className="wk-task-item">
-                                <div className='img-container'>
+                                <div className='wk-img-container'>
                                     <Image src="/images/fixed-form.png" className="wk-task-img" alt="" width={520} height={325} />
                                 </div>
                                 <div className="wk-task-info">
-                                    <h1 className="wk-ibm-plex-mono-bold">Fixed Forms</h1>
-                                    <p className="wk-ibm-plex-mono-light">Human evaluation of Key-Value pairs extracted by Computer Vision</p>
+                                    <h1 className="ibm-plex-mono-bold">Fixed Forms</h1>
+                                    <p className="ibm-plex-mono-light">Human evaluation of Key-Value pairs extracted by Computer Vision</p>
                                 </div>
                             </div>
                             <div className="wk-task-item">
-                                <div className='img-container'>
+                                <div className='wk-img-container'>
                                     <Image src="/images/verify-image.png" className="wk-task-img" alt="" width={520} height={325} />
                                 </div>
                                 <div className="wk-task-info">
-                                    <h1 className="wk-ibm-plex-mono-bold">Verify Image</h1>
-                                    <p className="wk-ibm-plex-mono-light">Label products, get human reviews for unsafe content, such as explicit adult content or violent content</p>
+                                    <h1 className="ibm-plex-mono-bold">Verify Image</h1>
+                                    <p className="ibm-plex-mono-light">Label products, get human reviews for unsafe content, such as explicit adult content or violent content</p>
                                 </div>
                             </div>
                         </div>
@@ -106,40 +106,40 @@ const page = () => {
                </div>
                <div className="wk-intro-container">
                 <div className="wk-intro-content">
-                    <h1 className="wk-ibm-plex-mono-bold intro-title">How to Earn in 4 Steps?</h1>
-                    <div className='four-step'>
+                    <h1 className="ibm-plex-mono-bold wk-intro-title">How to Earn in 4 Steps?</h1>
+                    <div className='wk-four-step'>
                     <div className="wk-intro-list-top">
                         <div className="wk-intro-item">
                             <Image src="/images/add.svg" alt="" width={76} height={76} />
                             <div className="wk-intro-des">
-                                <h1 className="wk-ibm-plex-mono-bold">Step 1</h1>
-                                <p className="wk-ibm-plex-mono-light">Register a new Metamask wallet</p>
+                                <h1 className="ibm-plex-mono-bold">Step 1</h1>
+                                <p className="ibm-plex-mono-light">Register a new Metamask wallet</p>
                             </div>
                         </div>
                         <div className="wk-intro-item">
                             <Image src="/images/wallet.svg" alt="" width={76} height={76} />
                             <div className="wk-intro-des">
-                                <h1 className="wk-ibm-plex-mono-bold">Step 2</h1>
-                                <p className="wk-ibm-plex-mono-light">Link your Metamask wallet to WorkEvo</p>
+                                <h1 className="ibm-plex-mono-bold">Step 2</h1>
+                                <p className="ibm-plex-mono-light">Link your Metamask wallet to WorkEvo</p>
                             </div>
                         </div>
                         <div className="wk-intro-item">
                             <Image src="/images/worker/choose-task.svg" alt="" width={76} height={76} />
                             <div className="wk-intro-des">
-                                <h1 className="wk-ibm-plex-mono-bold">Step 3</h1>
-                                <p className="wk-ibm-plex-mono-light">Choose task (Verify data or Image)</p>
+                                <h1 className="ibm-plex-mono-bold">Step 3</h1>
+                                <p className="ibm-plex-mono-light">Choose task (Verify data or Image)</p>
                             </div>
                         </div>
                         <div className="wk-intro-item">
                             <Image src="/images/worker/pig.svg" alt="" width={76} height={76} />
                             <div className="wk-intro-des">
-                                <h1 className="wk-ibm-plex-mono-bold">Step 4</h1>
-                                <p className="wk-ibm-plex-mono-light">Start working and receive rewards</p>
+                                <h1 className="ibm-plex-mono-bold">Step 4</h1>
+                                <p className="ibm-plex-mono-light">Start working and receive rewards</p>
                             </div>
                         </div>
                         
                     </div>
-                    <div className='intro-img'>
+                    <div className='wk-intro-img'>
                         <Image src="/images/worker/4-steps.png" alt="" width={555} height={541} />
                     </div>
                     </div>
@@ -148,8 +148,8 @@ const page = () => {
                {/* <div className="wk-communiti-2">
                     <div className="wk-com2-content">
                         <div className="wk-com2-left">
-                            <h1 className="wk-ibm-plex-mono-bold">Ready to build?</h1>
-                            <p className="wk-ibm-plex-mono-regular">A large, seemingly overwhelming task can sometimes be transformed into a set of smaller, more manageable microtasks that can each be accomplished independently.
+                            <h1 className="ibm-plex-mono-bold">Ready to build?</h1>
+                            <p className="ibm-plex-mono-regular">A large, seemingly overwhelming task can sometimes be transformed into a set of smaller, more manageable microtasks that can each be accomplished independently.
                             Contact us now!</p>
                         </div>
                         <div className="wk-com2-right">
@@ -157,24 +157,24 @@ const page = () => {
                         </div>
                     </div>
                </div> */}
-               <div className='wea-container'>
-                    <div className='wea-left'>
+               <div className='wk-wea-container'>
+                    <div className='wk-wea-left'>
                         <h1 className='ibm-plex-mono-bold'>WorkEvo token (WEA)</h1>
                         <h2 className='ibm-plex-mono-bold'>WorkEvo (REX) token is the main token of WorkEvo ecosystem</h2>
                         <p className='ibm-plex-mono-light'>REX is a BEP20 token, which is the main token of the WorkEvo ecosystem. Users will receive WorkEvo tokens for completing the tasks in Workevo.com</p>
                         <p className='ibm-plex-mono-light'>You will receive WEA when the system evaluates the task as completed.</p>
                         <div className='wea-btn'>
-                            <a href="#" className='buy-btn ibm-plex-mono-medium'>Buy WEA Now</a>
+                            <a href="#" className='wk-buy-btn ibm-plex-mono-medium'>Buy WEA Now</a>
                         </div>
                     </div>
-                    <div className='wea-right'>
+                    <div className='wk-wea-right'>
                         <Image src="/images/worker/token.png" alt="" width={650} height={780}/>
                     </div>
                </div>
                <div className="wk-footer-container">
                     <div className="wk-footer-left">
                         <Image src="/images/logo.png" alt="" width={250} height={50} className="wk-footer-logo"/>
-                        <h1 className="wk-ibm-plex-mono-bold">Connect with WorkEvo Community</h1>
+                        <h1 className="ibm-plex-mono-bold">Connect with WorkEvo Community</h1>
                         <div className="wk-footer-contact">
                             <a href="#"><Image src="/images/facebook.svg" className='icon-contact' alt="" width={40} height={40}/></a>
                             <a href="#"><Image src="/images/linkedin.svg" className='icon-contact' alt="" width={40} height={40}/></a>
@@ -191,15 +191,15 @@ const page = () => {
                             Cau Giay, Ha Noi</p>
                         </div>
                         <div className="wk-quick-link">
-                            <h1 className="wk-ibm-plex-mono-bold">Quick Links</h1>
-                            <a href="#" className="wk-ibm-plex-mono-regular">Home</a>
-                            <a href="#" className="wk-ibm-plex-mono-regular">Whitepaper</a>
-                            <a href="#" className="wk-ibm-plex-mono-regular">Tokenomics</a>
+                            <h1 className="ibm-plex-mono-bold">Quick Links</h1>
+                            <a href="#" className="ibm-plex-mono-regular">Home</a>
+                            <a href="#" className="ibm-plex-mono-regular">Whitepaper</a>
+                            <a href="#" className="ibm-plex-mono-regular">Tokenomics</a>
                         </div>
                     </div>
                </div>
                <div className="wk-copyright">
-                    <p className="wk-ibm-plex-mono-regular">Copyright ©  2024 WorkEvo Company Pte, Ltd.. All rights reserved.</p>
+                    <p className="ibm-plex-mono-regular">Copyright ©  2024 WorkEvo Company Pte, Ltd.. All rights reserved.</p>
                </div>
             </div>  
         </div>
